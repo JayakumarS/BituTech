405dbc2ad9e557501c44532b179ef55ade1ac242
StephenVivek <cvivek7080@gmail.com>
2022-05-21 11:08:35 +0530

    Modified some changes

--- src/main/java/com/bitutech/salesorder/SalesOrderQueryUtil.java
+++ src/main/java/com/bitutech/salesorder/SalesOrderQueryUtil.java
@@ -3,9 +3,9 @@ package com.bitutech.salesorder;
 public class SalesOrderQueryUtil {
 
 	public static final String INSERT_SALES_ORDER_HDR= "INSERT into sales_order_hdr (customer_code,terms_n_conditions,currency,exp_date_of_delivery,created_by,created_dt,sales_order_no,sales_quote_no,modified_by) "
-			+ " values(:customer,:termCondition,:currency,to_date(:deliveryDate,'YYYY-MM-DD'),'E0001',now(),:countValue,:salesQuoteNo,:modifiedBy) RETURNING sales_order_no as salesOrderNo";
+			+ " values(:customer,:termCondition,:currency,to_date(:deliveryDate,'dd/mm/yyyy'),'E0001',now(),:countValue,:salesQuoteNo,:modifiedBy) RETURNING sales_order_no as salesOrderNo";
 	public static final String GETCOUNT = "select * From fn_generate_reference_no('sales_order_hdr', 'sales_order_no','S','4')";
-	public static final String getSalesOrderList = "select customer_code as customer,terms_n_conditions as termCondition,currency as currency,to_char(exp_date_of_delivery,'YYYY-MM-DD') as deliveryDate,sales_order_no as countValue from sales_order_hdr";
+	public static final String getSalesOrderList = "select sales_order_no as countValue, customer_code as customer,terms_n_conditions as termCondition,currency as currency,to_char(exp_date_of_delivery,'YYYY-MM-DD') as deliveryDate,sales_order_no as countValue from sales_order_hdr";
 	public static final String SELECT_SALES_ORDER_HDR = "select customer_code as customer, terms_n_conditions as termCondition,currency as currency, to_char(exp_date_of_delivery,'YYYY-MM-DD') as deliveryDate,sales_quote_no as salesQuoteNo from sales_order_hdr where sales_order_no=?";
 	public static final String UPDATE_SALES_ORDER_HDR = "update sales_order_hdr set customer_code=:customer,sales_quote_no=:salesQuoteNo, terms_n_conditions=:termCondition,currency=:currency,exp_date_of_delivery=to_date(:deliveryDate,'YYYY-MM-DD') where sales_order_no=:countValue Returning sales_order_no as salesOrderNo";
 	public static final String DELETE_SALES_ORDER_HDR = "delete from sales_order_hdr where sales_order_no = ?";
