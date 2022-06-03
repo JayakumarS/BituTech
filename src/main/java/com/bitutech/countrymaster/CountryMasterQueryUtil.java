1541cbb5809da2a268ae17f8a863ceb7671590c5
StephenVivek <cvivek7080@gmail.com>
2022-06-03 18:11:28 +0530

    Country Master changes

--- src/main/java/com/bitutech/countrymaster/CountryMasterQueryUtil.java
+++ src/main/java/com/bitutech/countrymaster/CountryMasterQueryUtil.java
@@ -7,7 +7,7 @@ public class CountryMasterQueryUtil {
 	public static final String getCountryList = "select country_code as countryCode,country_name as countryName,currency_code as currency,country_two_code as clientType,is_active as isActive from country";
 	public static final String getCurrencyList = "select currency_code as id,currency_name as text from currency";
 	
-	public static final String UPDATE_COUNTRY_MASTER ="update country set country_name=:countryName,currency_code=:currency,country_two_code=:clientType,is_active=:isActive where country_code=:countryCode ";
+	public static final String UPDATE_COUNTRY_MASTER ="update country set country_code=:countryCode,country_name=:countryName,currency_code=:currency,country_two_code=:clientType,is_active=:isActive where country_code=:countryCode ";
 	public static final String SELECT_COUNTRY_MASTER = "select  country_code as countryCode,country_name as countryName,currency_code as currency,country_two_code as clientType,is_active as isActive from country where country_code = ? ";
 	public static final String DELETE_COUNTRY =" Delete from country where country_code = ?";
 	//public static final String SELECT_COUNTRY_MASTER = "select country_code as countryCode, country_name as countryName,currency_code as currency,is_active as isActive from country where country_code=?";
